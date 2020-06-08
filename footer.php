ac078e68ca0c54f42627fd477b58c429e2656c7e
unknown <pierre.leguidevais@viacesi.fr>
2020-06-08 16:14:21 +0200

    c'est carré mtn

--- footer.php
+++ footer.php
@@ -1,79 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Accueil</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
+
 <body>
 
-    <!-- Footer -->
-<footer>
+  <!-- Footer -->
+  <footer>
 
-  <!-- Footer Links -->
-  <div  class="container-fluid">
+    <!-- Footer Links -->
+    <div class="container-fluid">
 
-    <!-- Grid row -->
-    <div class="row">
+      <!-- Grid row -->
+      <div class="row">
 
-      <!-- Grid column -->
-      <div class="col mx-auto">
+        <!-- Grid column -->
+        <div class="col mx-auto">
 
-        <!-- Links -->
-        <h5 class="mt-4"> <a class="text-white click" href="#!">En savoir plus sur le CESI</a> </h5>
+          <!-- Links -->
+          <h5 class="mt-4"> <a class="text-white click" href="#!">En savoir plus sur le CESI</a> </h5>
+
+        </div>
+        <!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col mx-auto">
+
+          <!-- Links -->
+          <h5 class="mt-4"> <a class="text-white click" href="#!">Nos partenaires</a> </h5>
+
+        </div>
+        <!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col mx-auto">
+
+          <!-- Links -->
+          <h5 class="mt-4"> <a class="text-white click" href="#!">Adresse mail/Numéro de téléphone</a> </h5>
+
+        </div>
+        <!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col mx-auto">
+
+          <!-- Links -->
+          <h5 class="mt-4"> <a class="text-white click" href="#!">Conditions générales d'utilisation</a> </h5>
+
+        </div>
+        <!-- Grid column -->
 
       </div>
-      <!-- Grid column -->
-		<hr class="clearfix w-100 d-md-none">
-
-      <!-- Grid column -->
-      <div class="col mx-auto">
-
-        <!-- Links -->
-        <h5 class="mt-4"> <a class="text-white click" href="#!">Nos partenaires</a> </h5>
-
-      </div>
-      <!-- Grid column -->
-		<hr class="clearfix w-100 d-md-none">
-
-      <!-- Grid column -->
-      <div class="col mx-auto">
-
-        <!-- Links -->
-        <h5 class="mt-4"> <a class="text-white click" href="#!">Adresse mail/Numéro de téléphone</a> </h5>
-
-      </div>
-      <!-- Grid column -->
-		<hr class="clearfix w-100 d-md-none">
-
-      <!-- Grid column -->
-      <div class="col mx-auto">
-
-        <!-- Links -->
-        <h5 class="mt-4"> <a class="text-white click" href="#!">Conditions générales d'utilisation</a> </h5>
-
-      </div>
-      <!-- Grid column -->
+      <!-- Grid row -->
 
     </div>
-    <!-- Grid row -->
+    <!-- Footer Links -->
 
-  </div>
-  <!-- Footer Links -->
-
-</footer>
-<!-- Footer -->
+  </footer>
+  <!-- Footer -->
 </body>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
 </script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
 </script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+  integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
 </script>
+
 </html>
