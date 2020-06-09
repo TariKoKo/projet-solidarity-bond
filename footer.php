578445a20c889b41a5aedf4afd4083211f284359
unknown <pierre.leguidevais@viacesi.fr>
2020-06-09 10:15:22 +0200

    lien cesi et début page partenariat

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
         <div class="col mx-auto">
 
           <!-- Links -->
-          <h5 class="mt-4"> <a class="text-white click" href="#!">En savoir plus sur le CESI</a> </h5>
+          <h5 class="mt-4"> <a class="text-white click" href="https://ecole-ingenieurs.cesi.fr/">En savoir plus sur le CESI</a> </h5>
 
         </div>
         <!-- Grid column -->
@@ -33,7 +33,7 @@
         <div class="col mx-auto">
 
           <!-- Links -->
-          <h5 class="mt-4"> <a class="text-white click" href="#!">Nos partenaires</a> </h5>
+          <h5 class="mt-4"> <a class="text-white click" href="partenariats.php">Nos partenaires</a> </h5>
 
         </div>
         <!-- Grid column -->
---
+++ partenariats.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partenariats</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+</head>
+
+<body id="partenariats">
+        <div class="container-fluid">
+            <div class="row mt-5">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <a href="index.php"><img src="assets/image/Logo_cesi.png"></a>
+                </div>
+                <div class="col-8 mt-4">
+                    <h1>Projet Solidarity-Bond</h1>
+                </div>
+                <div> <?php include('boutonConnexion.php'); ?> </div>
+            </div>
+        </div>
+    <main></main>
+    <footer id="footer2">
+        <?php include('footer.php'); ?>
+    </footer>
+</body>
+
+</html>
